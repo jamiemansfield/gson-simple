389a8754fe6a5dbea2d50d650585f3a14ff664c3
Jamie Mansfield <jmansfield@cadixdev.org>
2019-11-06 10:13:57 +0000

    Add type adapter factory for lowercase enums
    Signed-off-by: Jamie Mansfield <jmansfield@cadixdev.org>

---
+++ src/main/java/me/jamiemansfield/gsonsimple/LowercaseEnumTypeAdapterFactory.java
@@ -0,0 +1,89 @@
+/*
+ * This file is part of gson-simple, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Jamie Mansfield <https://www.jamiemansfield.me/>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package me.jamiemansfield.gsonsimple;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A {@link TypeAdapterFactory type adapter factory} to allow enum's
+ * to be serialised/de-serialised with their lowercase names.
+ *
+ * @author Jamie Mansfield
+ * @since 0.1.0
+ */
+public class LowercaseEnumTypeAdapterFactory implements TypeAdapterFactory {
+
+    @Override
+    public <T> TypeAdapter<T> create(final Gson gson, final TypeToken<T> type) {
+        final Class<T> rawType = (Class<T>) type.getRawType();
+        if (!rawType.isEnum()) return null;
+
+        final Map<String, T> values = new HashMap<>();
+        for (final T constant : rawType.getEnumConstants()) {
+            values.put(toLowercase(constant), constant);
+        }
+
+        return new TypeAdapter<T>() {
+            @Override
+            public void write(final JsonWriter out, final T value) throws IOException {
+                if (value == null) {
+                    out.nullValue();
+                    return;
+                }
+                out.value(toLowercase(value));
+            }
+
+            @Override
+            public T read(final JsonReader in) throws IOException {
+                if (in.peek() == JsonToken.NULL) {
+                    in.nextNull();
+                    return null;
+                }
+                return values.get(in.nextString());
+            }
+        };
+    }
+
+    private static String toLowercase(final Object obj) {
+        if (obj instanceof Enum) {
+            return ((Enum) obj).name().toLowerCase();
+        }
+        // This is just a fallback, though it should NEVER
+        // be reached.
+        return obj.toString().toLowerCase();
+    }
+
+}
